1eaef844d6a4742b96a2609b2c50307f828e2743
Gregory <g.vandenende@live.nl>
2016-02-15 12:26:01 +0100

    404 pagina gemaakt en .htaccess bestand voor de link.

---
+++ error.php
@@ -0,0 +1,23 @@
+
+    <!DOCTYPE html>
+    <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+    <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+    <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+    <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<body>
+<?php
+include_once "header.php";
+?>
+<div>
+    <br>
+    <br>
+    <br>
+    <br>
+<center><h1>404 Pagina kon niet gevonden worden :(</h1></center><br>
+<center><h1>Deze pagina bestaat(nog) niet!</h1></center>
+</div>
+</body>
+<?PHP
+include_once "footer.php";
+
+?>
