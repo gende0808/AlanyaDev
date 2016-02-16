b30329115ffa42b2d02e91ccecf509e2d1dc30ff
jimebbelaar <jim.ebbelaar@gmail.com>
2016-02-16 09:16:16 +0100

    Image for nav + grill.php + datatable.js - Jim

---
+++ grill.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+<?PHP
+include_once "header.php";
+?>
+<body>
+
+<div id="main-header">
+    <div class="logo">
+        <hr>
+        <center><img src="images/testlogo2.png"></center>
+    </div>
+</div>
+
+
+
+<div class="row">
+    <div class="col-md-4">
+    </div>
+    <div class="col-md-4">
+        </div>
+    <div class="col-md-4">
+    </div>
+</div>
+
+
+<?PHP
+include_once "footer.php";
+// include_once "sideshoppinglist.php";
+?>
+
+
+<!-- The scroll to top feature -->
+<div class="scroll-top-wrapper ">
+  <span class="scroll-top-inner">
+<i class="fa fa-2x fa-arrow-circle-up"></i>
+  </span>
+</div>
+<script src="js/vendor/jquery-1.11.0.min.js"></script>
+<script src="js/vendor/jquery.gmap3.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/login.js"></script>
+<script src="js/scrolltop.js"></script>
+
+</body>
+</html>
--- header.php
+++ header.php
@@ -21,6 +21,8 @@
     <link rel="stylesheet" href="css/border.css">
         <link href="css/demo-page.css" rel="stylesheet" media="all">
         <link href="css/hover.css" rel="stylesheet" media="all">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/t/bs/jq-2.2.0,dt-1.10.11/datatables.min.css"/>
+    <script type="text/javascript" src="https://cdn.datatables.net/t/bs/jq-2.2.0,dt-1.10.11/datatables.min.js"></script>
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" media="all">
     <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
     <script src="js/jquery-1.9.1.min.js"></script>
