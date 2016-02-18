c7d0fd2e529d6c3365e88033799d7485313e5eee
FrozenPeppers <joris-ebbelaar@hotmail.nl>
2016-02-18 13:18:56 +0100

    Keukenaccount

---
+++ keukenaccount.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+<head>
+    <?php header("Content-Type: text/html; charset=utf-8"); ?>
+    <meta charset="utf-8">
+    <title>Alanya Krommenie</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/templatemo_style.css">
+    <link rel="stylesheet" href="css/templatemo_misc.css">
+    <link rel="stylesheet" href="css/flexslider.css">
+    <link rel="stylesheet" href="css/testimonails-slider.css">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/orange.css">
+    <link rel="stylesheet" href="css/scrolltop.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/sidebar.css">
+    <link rel="stylesheet" href="css/sideshop.css">
+    <link rel="stylesheet" href="css/border.css">
+    <link href="css/demo-page.css" rel="stylesheet" media="all">
+    <link href="css/hover.css" rel="stylesheet" media="all">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/t/bs/jq-2.2.0,dt-1.10.11/datatables.min.css"/>
+</head>
+<div class="container">
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-condensed">
+                        <thead>
+                        <tr>
+                            <td><strong>Product Naam</strong></td>
+                            <td class="text-center"><strong>Toevoegingen</strong></td>
+                            <td class="text-center"><strong>Prijs</strong></td>
+                            <td class="text-center"><strong>Aantal</strong></td>
+                            <td class="text-right"><strong>Totaal</strong></td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Pizza Boromea</td>
+                            <td class="highrow"></td>
+                            <td class="text-center">€8,-</td>
+                            <td class="text-center">2</td>
+                            <td class="text-right">€16,-</td>
+                        </tr>
+                        <tr>
+                            <td>Kapsalon</td>
+                            <td class="highrow"></td>
+                            <td class="text-center">€6,-</td>
+                            <td class="text-center">1</td>
+                            <td class="text-right">€6,-</td>
+                        </tr>
+                        <tr>
+                            <td>Turkse Pizza</td>
+                            <td class="highrow"></td>
+                            <td class="text-center">€2,50</td>
+                            <td class="text-center">4</td>
+                            <td class="text-right">€10,-</td>
+                        </tr>
+                        <tr>
+                            <td class="highrow"></td>
+                            <td class="highrow"></td>
+                            <td class="highrow"></td>
+                            <td class="highrow text-center"><strong>Sub totaal</strong></td>
+                            <td class="highrow text-right">€32,-</td>
+                        </tr>
+                        <tr>
+                            <td class="highrow"></td>
+                            <td class="emptyrow"></td>
+                            <td class="emptyrow"></td>
+                            <td class="emptyrow text-center"><strong>Verzendkosten</strong></td>
+                            <td class="emptyrow text-right">€0,-</td>
+                        </tr>
+                        <tr>
+                            <td class="highrow"></td>
+                            <td class="emptyrow"></td>
+                            <td class="emptyrow"></td>
+                            <td class="emptyrow text-center"><strong>Totaal</strong></td>
+                            <td class="emptyrow text-right">$32,-</td>
+                        </tr>
+                        <tr>
+                            <td class="emptyrow">
+                                <strong>
+                                    <p>Naam</p>
+                                    <p>Adres</p>
+                                    <p>Tel</p>
+                                </strong></td>
+                            <td class="emptyrow">
+                                <strong>
+                                    <p>Boudewijn Bos</p>
+                                    <p>Padlaan 9, Krommenie</p>
+                                    <p>075-6874522</p>
+                                </strong></td>
+
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-condensed">
+                        <thead>
+                        <tr>
+                            <td><strong>Product Naam</strong></td>
+                            <td class="text-center"><strong>Toevoegingen</strong></td>
+                            <td class="text-center"><strong>Prijs</strong></td>
+                            <td class="text-center"><strong>Aantal</strong></td>
+                            <td class="text-right"><strong>Totaal</strong></td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Pizza Boromea</td>
+                            <td class="highrow"></td>
+                            <td class="text-center">€8,-</td>
+                            <td class="text-center">2</td>
+                            <td class="text-right">€16,-</td>
+                        </tr>
+                        <tr>
+                            <td>Kapsalon</td>
+                            <td class="highrow"></td>
+                            <td class="text-center">€6,-</td>
+                            <td class="text-center">1</td>
+                            <td class="text-right">€6,-</td>
+                        </tr>
+                        <tr>
+                            <td>Turkse Pizza</td>
+                            <td class="highrow"></td>
+                            <td class="text-center">€2,50</td>
+                            <td class="text-center">4</td>
+                            <td class="text-right">€10,-</td>
+                        </tr>
+                        <tr>
+                            <td class="highrow"></td>
+                            <td class="highrow"></td>
+                            <td class="highrow"></td>
+                            <td class="highrow text-center"><strong>Sub totaal</strong></td>
+                            <td class="highrow text-right">€32,-</td>
+                        </tr>
+                        <tr>
+                            <td class="highrow"></td>
+                            <td class="emptyrow"></td>
+                            <td class="emptyrow"></td>
+                            <td class="emptyrow text-center"><strong>Verzendkosten</strong></td>
+                            <td class="emptyrow text-right">€0,-</td>
+                        </tr>
+                        <tr>
+                            <td class="highrow"></td>
+                            <td class="emptyrow"></td>
+                            <td class="emptyrow"></td>
+                            <td class="emptyrow text-center"><strong>Totaal</strong></td>
+                            <td class="emptyrow text-right">$32,-</td>
+                        </tr>
+                        <tr>
+                            <td class="emptyrow">
+                                <strong>
+                                    <p>Adres</p>
+                                    <p>Tel</p>
+                                </strong></td>
+                            <td class="emptyrow">
+                                <strong>
+                                    <p>Padlaan 9, Krommenie</p>
+                                    <p>075-6874522</p>
+                                </strong></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+
+<!-- The scroll to top feature -->
+<div class="scroll-top-wrapper ">
+  <span class="scroll-top-inner">
+<i class="fa fa-2x fa-arrow-circle-up"></i>
+  </span>
+</div>
+
+
+</body>
+</html>
