2a47d95975edcaa1aaa4b0d2dbd07b4ab8d84364
davidencko <davidencko64@gmail.com>
2016-02-18 15:40:04 +0100

    adminaccount toegevoegd

---
+++ adminaccount.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+<head>
+    <?php header("Content-Type: text/html; charset=utf-8"); ?>
+    <meta charset="utf-8">
+    <title>Alanya Krommenie</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/templatemo_style.css">
+    <link rel="stylesheet" href="css/templatemo_misc.css">
+    <link rel="stylesheet" href="css/flexslider.css">
+    <link rel="stylesheet" href="css/testimonails-slider.css">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/orange.css">
+    <link rel="stylesheet" href="css/scrolltop.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/sidebar.css">
+    <link rel="stylesheet" href="css/sideshop.css">
+    <link rel="stylesheet" href="css/border.css">
+    <link href="css/demo-page.css" rel="stylesheet" media="all">
+    <link href="css/hover.css" rel="stylesheet" media="all">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/t/bs/jq-2.2.0,dt-1.10.11/datatables.min.css"/>
+</head>
+<br>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Zoek product" style="border:1px solid lightgrey">
+            </div><!-- /input-group -->
+        </div><!-- /.col-lg-6 -->
+        <div class="col-lg-6" >
+            <div class="input-group" style="float: right; margin-top: 25px">
+                <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-plus"></span> Voeg product toe</button>
+            </div><!-- /input-group -->
+        </div><!-- /.col-lg-6 -->
+    </div><!-- /.row -->
+</div>
+<br>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table class="table table-condensed">
+                            <thead>
+                            <tr>
+                                <td><strong>Product ID</strong></td>
+                                <td class="text-center"><strong>ProductNummer</strong></td>
+                                <td class="text-center"><strong>ProductNaam</strong></td>
+                                <td class="text-center"><strong>ProductOmschrijving</strong></td>
+                                <td class="text-right"><strong>Prijs</strong></td>
+                                <td class="text-right"><strong>Optie</strong></td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td class="text-center">1</td>
+                                <td class="text-center">Pizza Hawaii</td>
+                                <td class="text-center">Ananas en Ham</td>
+                                <td class="text-right">€16,00</td>
+                                <td class="text-right"><a href="http://localhost/AlanyaDev/adminaccount.php">Verander</a></td>
+                                <td class="text-right"><a href="http://localhost/AlanyaDev/adminaccount.php">Verwijder</a></td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td class="text-center">2</td>
+                                <td class="text-center">Pizza Salami</td>
+                                <td class="text-center">Salami en kaas</td>
+                                <td class="text-right">€14,00</td>
+                                <td class="text-right"><a href="http://localhost/AlanyaDev/adminaccount.php">Verander</a></td>
+                                <td class="text-right"><a href="http://localhost/AlanyaDev/adminaccount.php">Verwijder</a></td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td class="text-center">3</td>
+                                <td class="text-center">Pizza margarita</td>
+                                <td class="text-center">Kaas</td>
+                                <td class="text-right">€9,00</td>
+                                <td class="text-right"><a href="http://localhost/AlanyaDev/adminaccount.php">Verander</a></td>
+                                <td class="text-right"><a href="http://localhost/AlanyaDev/adminaccount.php">Verwijder</a></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+</div>
+
+<!-- The scroll to top feature -->
+<div class="scroll-top-wrapper ">
+  <span class="scroll-top-inner">
+<i class="fa fa-2x fa-arrow-circle-up"></i>
+  </span>
+</div>
+
+
+</body>
+</html>
