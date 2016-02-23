b526a836cf7be4ba1a59d757205c2cf557abe470
Gregory <g.vandenende@live.nl>
2016-02-23 11:58:56 +0100

    test.php voor iedereen

---
+++ test.php
@@ -0,0 +1,23 @@
+<?PHP include_once "header.php"; ?>
+
+
+
+
+<?PHP
+
+include_once "classes/Product1.php";
+
+$product10 = new Product1;
+$product14 = new Product1;
+
+$product14->read(31);
+
+
+
+?>
+
+
+
+
+
+<?PHP include_once "footer.php" ?>
