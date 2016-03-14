0dd536dff69de5c86c3a90e91f665be8db3303d0
davidencko <davidencko64@gmail.com>
2016-03-14 11:10:07 +0100

    Login form functioneel maken

--- loginmodal.php
+++ loginmodal.php
@@ -9,34 +9,33 @@
             </div> <!-- /.modal-header -->
 
             <div class="modal-body">
-                <form role="form">
+                <form role="form" method="post" action="login.php">
                     <div class="form-group">
                         <div class="input-group">
                             <label for="uLogin" class="input-group-addon orange glyphicon glyphicon-user"></label>
-                            <input type="text" class="form-control" id="uLogin" placeholder="E-mail">
+                            <input type="text" class="form-control" id="uLogin" name="loginemail" placeholder="E-mail">
                         </div>
                     </div> <!-- /.form-group -->
 
                     <div class="form-group">
                         <div class="input-group">
                             <label for="uPassword" class="input-group-addon orange glyphicon glyphicon-lock"></label>
-                            <input type="password" class="form-control" id="uPassword" placeholder="Wachtwoord">
+                            <input type="password" class="form-control" id="uPassword" name="loginpass" placeholder="Wachtwoord">
                         </div> <!-- /.input-group -->
                     </div> <!-- /.form-group -->
                     <p>Nog geen account? <a href="register.php">Registreren</a></p>
-                </form>
 
             </div> <!-- /.modal-body -->
 
             <div class="modal-footer">
-                <button class="form-control btn orange" style="color: white;">Login</button>
+                <button class="form-control btn orange" type="submit" style="color: white;">Login</button>
                 <div class="progress">
                     <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="1" aria-valuemin="1" aria-valuemax="100" style="width: 0%;">
                         <span class="sr-only">progress</span>
                     </div>
                 </div>
-
             </div> <!-- /.modal-footer -->
+            </form>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
