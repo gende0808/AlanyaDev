d23ae40a772b7e3ba861bfbf1021ce4e3d2bee37
jimebbelaar <jim.ebbelaar@gmail.com>
2016-03-14 13:26:54 +0100

    Discountmanager

--- discountmanager.php
+++ discountmanager.php
@@ -1,10 +1,32 @@
 <?php
 include_once 'header2.php';
-/**
- * Created by PhpStorm.
- * User: Gregory
- * Date: 14-3-2016
- * Time: 09:18
- */
 
-echo 'bestaande acties: ';
+?>
+<div class="container text-center" style="margin-top: 50px;">
+    <div class="col-md-12 col-md-offset-0 text-center" style="margin-top: 50px">
+        <div class="col-md-12 col-md-offset-0 text-center">
+            <div id="placehere" style="margin-bottom: 5%">
+            </div>
+        </div>
+
+        <table class='table table-striped table-hover table-responsive'>
+            <thead>
+            <tr>
+                <th class='text-center'>Actienaam</th>
+                <th class='text-center'>Begin-datum</th>
+                <th class='text-center'>Eind-datum</th>
+                <th class='text-center'>Omschrijving</th>
+                <th class='text-center'>Verwijderen</th>
+            </tr>
+            </thead>
+            <tbody>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            </tbody>
+        </table>
+    </div>
+</div>
+</div>
--- login.php
+++ login.php
@@ -5,11 +5,7 @@ include_once "classes/Account.php";
 //$email = $_POST['loginemail'];
 //$password = $_POST['loginpass'];
 
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
+
     // Password : 874e0cc1eb15bdaf323800180d19d69fe4ba2cedd2954ab332e25a2a85ab3248
         try
         {
