2179d5b8800c489d4f1c6b40f62227cfd9469cbf
jimebbelaar <jim.ebbelaar@gmail.com>
2016-03-15 09:02:35 +0100

    login.php wip

--- login.php
+++ login.php
@@ -9,9 +9,7 @@ include_once "classes/AccountList.php";
 
 $email = $_POST['loginemail'];
 $password = $_POST['loginpass'];
-
-
-    // Password : 874e0cc1eb15bdaf323800180d19d69fe4ba2cedd2954ab332e25a2a85ab3248
+$waarde = false;
         try
         {
                 $accountlist = new AccountList($DB_con);
@@ -22,19 +20,23 @@ $password = $_POST['loginpass'];
 
                         if($email === $accemail && $password === $accpassword){
                                 echo $account->getUserfirstname().' '.$account->getUserlastname();
-                                echo " er komt een wachtwoord overeen!";
-                                echo " het werkt!";
-                                return $waarde = 'appel';
+                                $waarde = true;
+                                exit;
                         }
                 }
+
         }
         catch(PDOException $e)
         {
-
+                echo 'er is iets fout gegaan';
         }
-        if ($waarde = 'appel')
+        if (!$waarde)
         {
                 echo "fout wachtwoord/gebruikersnaam";
+        }
+        if ($waarde)
+        {
+                echo "er komt een wachtwoord overeen!";
         }
 
 
