1e17e3441a5c3047e3b0bde77177e030b93dd81e
davidencko <davidencko64@gmail.com>
2016-03-25 13:48:45 +0100

    Aanpassing aan de vorige push

--- login.php
+++ login.php
@@ -20,15 +20,17 @@ try {
 
 
         if ($email === $accemail && password_verify($password, $accpassword)) {
-            //als de invoer van email en wachtwoord overeenkomen met een account in de database word
-            //de sessie 'logged' op true gezet en de sessie 'user_info' word gevuld met gegevens van de gebruiker.
-            $_SESSION['logged'] = true;
-            $_SESSION['user_info'] = $account->getUserInfo();
-            $_SESSION['account_id'] = $account->getUserid();
-            $_SESSION['city_id'] = $account->getUsercityid();
-            header('Location: index.php');
-        }else {
-            header("Location: account_not_activated.php");
+            if ($account->getstatus() == "Y") {
+                //als de invoer van email en wachtwoord overeenkomen met een account in de database word
+                //de sessie 'logged' op true gezet en de sessie 'user_info' word gevuld met gegevens van de gebruiker.
+                $_SESSION['logged'] = true;
+                $_SESSION['user_info'] = $account->getUserInfo();
+                $_SESSION['account_id'] = $account->getUserid();
+                $_SESSION['city_id'] = $account->getUsercityid();
+                header('Location: index.php');
+            } else {
+                header("Location: account_not_activated.php");
+            }
         }
     }
 
