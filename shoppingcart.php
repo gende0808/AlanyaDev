1362f6feff181365c7c66c1ad598986a4502ea7b
jimeb <jim.ebbelaar@gmail.com>
2016-06-15 09:28:35 +0200

    shoppingcart

--- shoppingcart.php
+++ shoppingcart.php
@@ -35,86 +35,17 @@ if (isset($_SESSION['productencart'])) {
 }
 
 ?>
-
-<body>
-<!--[if lt IE 7]>
-<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser
-    today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better
-    experience this site.</p>
-<![endif]-->
-<div id="main-header">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="logo">
-                    <center><img src="images/testlogo3.png"></center>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</header>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h4 class="modal-title" id="myModalLabel">Log in</h4>
-            </div>
-            <!-- /.modal-header -->
-
-            <div class="modal-body">
-                <form role="form">
-                    <div class="form-group">
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="uLogin" placeholder="Gebruikersnaam">
-                            <label for="uLogin" class="input-group-addon orange glyphicon glyphicon-user"></label>
-                        </div>
-                    </div>
-                    <!-- /.form-group -->
-
-                    <div class="form-group">
-                        <div class="input-group">
-                            <input type="password" class="form-control" id="uPassword" placeholder="Wachtwoord">
-                            <label for="uPassword" class="input-group-addon orange glyphicon glyphicon-lock"></label>
-                        </div>
-                        <!-- /.input-group -->
-                    </div>
-                    <!-- /.form-group -->
-
-                    <p>Nog geen account? <a href="register.php">Registreer</a></p>
-                </form>
-
-            </div>
-            <!-- /.modal-body -->
-
-            <div class="modal-footer">
-                <button class="form-control btn orange">Ok</button>
-
-                <div class="progress">
-                    <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="1"
-                         aria-valuemin="1" aria-valuemax="100" style="width: 0%;">
-                        <span class="sr-only">progress</span>
-                    </div>
-                </div>
-            </div>
-            <!-- /.modal-footer -->
-
-        </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-</div>
-<!-- /.modal -->
-<!-- /originalsliderplace -->
 <?php
 if ($productensession) {
 
 
     ?>
+
     <div class="container">
         <div class="row">
+    <div class="logo">
+        <center><img src="images/testlogo3.png"></center>
+    </div>
             <div class="col-sm-12 col-md-12">
                 <table class="table">
                     <thead>
@@ -310,7 +241,6 @@ if ($productensession) {
 }
 
 include_once "footer.php";
-// include_once "sideshoppinglist.php";
 ?>
 
 <!-- The scroll to top feature -->
