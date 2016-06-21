784c06b11591dfc73fbf05083beee0eeb023efec
Gregory <g.vandenende@live.nl>
2016-06-21 10:10:22 +0200

    bugfix

--- keukenaccount.php
+++ keukenaccount.php
@@ -237,7 +237,6 @@ foreach ($listofbestellingen as $bestelling){
 <!-- The scroll to top feature -->
 <div class="scroll-top-wrapper ">
   <span class="scroll-top-inner">
-  <span class="scroll-top-inner">
 <i class="fa fa-2x fa-arrow-circle-up"></i>
   </span>
 </div>
